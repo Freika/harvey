b87362df73e260caa450676da86cfb00f1ce5436
Freika <iamfrey@gmail.com>
2016-10-06 19:55:18 +0700

    Update json_url function.

--- web/models/realm.ex
+++ web/models/realm.ex
@@ -16,15 +16,15 @@ defmodule Harvey.Realm do
     |> validate_required([:name])
   end
 
-  def json_url do
+  def json_url(realm_name) do
     api_key = Application.get_env(:harvey, :bnet_api_key)
     # response = HTTPotion.get("https://eu.api.battle.net/wow/auction/data/#{realm_name}?locale=en_GB&apikey=#{api_key}")
-    response = HTTPotion.get("https://eu.api.battle.net/wow/auction/data/eversong?locale=en_GB&apikey=#{api_key}")
+    response = HTTPotion.get("https://eu.api.battle.net/wow/auction/data/#{realm_name}?locale=en_GB&apikey=#{api_key}")
     {status, body} = JSON.decode(response.body)
     List.first(body["files"])["url"]
   end
 
-  def read_auction(url) do
+  def read_auction(url, realm_name) do
     response = HTTPotion.get(url, [timeout: :infinity])
     json = JSON.decode(response.body)
     {status, body} = json
